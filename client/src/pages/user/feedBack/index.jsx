52fefd2cb7a26091f212336b25a05c657c361e72
解成博 <chengbo_x@163.com>
2019-10-20 12:33:18 +0800

    add 帮助

--- client/src/pages/user/feedBack/index.jsx
+++ client/src/pages/user/feedBack/index.jsx
@@ -24,6 +24,11 @@ export default class FeedBack extends Component {
                     title='1. 首页红黄绿小薯条代表什么？'
                     note='答：绿色代表正计时；黄色代表倒计时；红色代表倒计时已超时。'
                 ></AtCard>
+                <View style={{ height: Taro.pxTransform(20) }}></View>
+                <AtCard
+                    title='2. 如何删除或重新编辑事件？'
+                    note='答：长按首页事件试试哦...'
+                ></AtCard>
                 <View className='feedback__footer'>
                     <AtButton type='secondary' circle={true} openType='feedback'>其他问题反馈</AtButton>
                     <View style={{height: Taro.pxTransform(20)}}></View>
